137779f510ab8e4717c5dbdbe03c8f52a5f89bcf
GMShahzaib <mirza.shahzaib@theaimegroup.com>
2024-11-13 12:08:01 +0500

    added vercel config

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, resp) => {
+
+    resp.status(200).send({ message: "success", status: 200 })
+
+})
+
 app.use('/login', authRoutes);
 app.use('/users', userRoutes);
 app.use('/docs', outport.serve());
