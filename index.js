41d9bd3c1bf4c9dafc2ac76458172304874b2f20
GMShahzaib <mirza.shahzaib@theaimegroup.com>
2024-12-21 11:06:28 +0500

    updated outport version

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from './src/app.js';
 
-const PORT = process.env.PORT || 8085;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`Application is running on port ${PORT}.`);
